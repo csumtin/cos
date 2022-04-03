489fcc4cdab16bbe200550d2c5a95922ff06dcdd
csumtin <csumtin@protonmail.com>
2022-04-03 09:48:03 +0100

    remove unused

--- post-install1.sh
+++ post-install1.sh
@@ -10,23 +10,7 @@ fi
 DEBIAN_FRONTEND=noninteractive apt -y install --no-install-recommends pulseaudio
 
 # bluetooth
-DEBIAN_FRONTEND=noninteractive apt -y install --no-install-recommends gnome-bluetooth pulseaudio-module-bluetooth
-
-# for cua
-DEBIAN_FRONTEND=noninteractive apt -y install --no-install-recommends python3-pip python3-setuptools linux-headers-$(uname -r) python3-dev gcc
+# DEBIAN_FRONTEND=noninteractive apt -y install --no-install-recommends gnome-bluetooth pulseaudio-module-bluetooth
 
 # for cas
 DEBIAN_FRONTEND=noninteractive apt -y install --no-install-recommends steghide gnupg pwgen xclip
-
-cd /home/c
-git clone https://github.com/csumtin/cua.git
-git clone https://github.com/csumtin/ccs.git
-git clone https://github.com/csumtin/cpl.git
-chown -R c:c cua
-chown -R c:c ccs
-chown -R c:c cpl
-
-cd /home/c/cua
-pip install evdev
-cp cua0.service /etc/systemd/system/
-systemctl enable cua0
